b2c7e5422c850f5b7e12a51b6ed69a7b8184b732
Andrew L. Van Slaars <avanslaars@gmail.com>
2015-07-19 12:24:28 -0400

    Added app.set for view engine and views directory

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var express = require('express'),
 
 app.engine('html',swig.renderFile)
 
+app.set('view engine', 'html')
+app.set('views',__dirname + '/views')
+
 app.get('/',function(req,res){
   res.send('Hello World')
 })
